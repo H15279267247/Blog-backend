1223fac308a73e846b7e7efa72555c7e78006191
Hong <15279267247@163.com>
2025-08-06 14:16:26 +0800

    Blog
    Signed-off-by: Hong <15279267247@163.com>

---
+++ src/main/java/com/zhieasy/blog/entity/user.java
@@ -0,0 +1,9 @@
+package com.zhieasy.blog.entity;
+
+public class user {
+    private String name;
+    private String password;
+    private String email;
+    private String phone;
+    private String address;
+}
