bae0a7fc8ac51a7c0fdfd4c61bead1c200efaac1
Hong <15279267247@163.com>
2025-08-06 14:16:26 +0800

    Blog
    Signed-off-by: Hong <15279267247@163.com>

--- src/main/java/com/zhieasy/blog/Hello.java
+++ src/main/java/com/zhieasy/blog/Hello.java
@@ -3,5 +3,6 @@ package com.zhieasy.blog;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello,java");
+        System.out.println("hello,world");
     }
 }
